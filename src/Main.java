b92b20932eeee5f0abfc0d4e04624371ff310d52
Dania Mohammed <92473891+xDania@users.noreply.github.com>
2021-12-03 19:48:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,15 @@
+package BrickBreaker;
+import javax.swing.JFrame;
 
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.setSize(700,600);
+        obj.setTitle("Brick Breaker Game");
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        obj.setVisible(true);
+    }
+}
